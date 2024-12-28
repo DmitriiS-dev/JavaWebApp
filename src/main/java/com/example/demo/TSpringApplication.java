75fd8d3229a31faee638e4d53745538e3838eaea
DmitriiS-dev <dmitrii.sizikov577@gmail.com>
2024-12-28 20:51:33 +0000

    Spring Project Setup

---
+++ src/main/java/com/example/demo/TSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TSpringApplication.class, args);
+	}
+
+}
