7e84fec7e2ebc280c14191437b86ef1d283d2e71
DmitriiS-dev <dmitrii.sizikov577@gmail.com>
2024-12-28 22:33:03 +0000

    Spring Project: Clean Setup - Service and Controller Configured

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,24 +1,26 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
     //Get Restful Endpoint
     @GetMapping
-    public List<Student> hello(){
-        List<Student> l1 = new ArrayList<Student>();
-        Student s = new Student(1L,"Marian","marian@gmail.com",21, LocalDate.of(2003, Month.JANUARY, 5));
-        l1.add(s);
-        return l1;
+    public List<Student> getStudents(){
+        return studentService.getStudents();
     }
 }
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    //Get Restful Endpoint
+    @GetMapping
+    public List<Student> getStudents(){
+//        Coming for a Database:
+        List<Student> l1 = new ArrayList<Student>();
+        Student s = new Student(1L,"Marian","marian@gmail.com",21, LocalDate.of(2003, Month.JANUARY, 5));
+        l1.add(s);
+        return l1;
+    }
+}
