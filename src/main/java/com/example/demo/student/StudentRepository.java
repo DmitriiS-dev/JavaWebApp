8b6f036a600c297c6046eeb2e46c5c7eef59a883
DmitriiS-dev <dmitrii.sizikov577@gmail.com>
2024-12-29 13:14:31 +0000

    Post Functionality Implemented

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,11 +1,19 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 //Interface Resposible for the Data Access:
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
 
+
+//this is JBQL and not straight sql (Student is the class not the DB)
+    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+    //    Find a User By Email - Transformed to an SQL
+    Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,10 +1,12 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -21,5 +23,18 @@ public class StudentService {
     public List<Student> getStudents(){
 //        Coming for a Database:
         return studentRepository.findAll();
+    }
+
+//    Post Restful Endpoint
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("Email Taken");
+        }
+
+        studentRepository.save(student);
+        System.out.println(student);
+
     }
 }
