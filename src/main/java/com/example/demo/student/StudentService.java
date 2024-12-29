1750bbe11ba7c52ebeba9ca3887a8108842bea73
DmitriiS-dev <dmitrii.sizikov577@gmail.com>
2024-12-29 14:09:48 +0000

    Update Functionality Implemented

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,12 @@
 package com.example.demo.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -38,7 +39,7 @@ public class StudentService {
 
     }
 
-//    Deletes API Endpoint
+//    Delete API Endpoint
     public void deleteStudent(Long studentId) {
         boolean exists = studentRepository.existsById(studentId);
 
@@ -46,5 +47,27 @@ public class StudentService {
             throw new IllegalStateException("student with id "+ studentId);
         }
         studentRepository.deleteById(studentId);
+    }
+
+//    Update API Endpoint
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "Student with Id "+ studentId + " was not found!"
+                )
+        );
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()){
+                throw new IllegalStateException("Email Taken!");
+            }
+            System.out.println("should update the Email");
+            student.setEmail(email);
+        }
     }
 }
