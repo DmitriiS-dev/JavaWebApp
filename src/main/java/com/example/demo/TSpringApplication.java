69296f853a15b0b92bda1d77d65297fa5bb269fb
DmitriiS-dev <dmitrii.sizikov577@gmail.com>
2024-12-29 02:22:58 +0000

    Local Postgres Database ALL Set Up and Running User is able to Create a Table and Insert a row through Spring

--- src/main/java/com/example/demo/TSpringApplication.java
+++ src/main/java/com/example/demo/TSpringApplication.java
@@ -1,15 +1,7 @@
 package com.example.demo;
 
-import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class TSpringApplication {
@@ -17,6 +9,4 @@ public class TSpringApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TSpringApplication.class, args);
 	}
-
-
 }
